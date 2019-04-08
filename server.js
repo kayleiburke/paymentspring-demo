23242a461b00fefa5903d341c5ce475e9f8f3442
Kaylei Burke <kaylei.burke@gmail.com>
2019-04-07 22:43:10 -0500

    Configure Express so that it handles vue-router history mode in production
    See https://stackoverflow.com/a/53105299/4714425

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-// server.js
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
+const express = require('express');
+const path = require('path');
+const history = require('connect-history-api-fallback');
+
+const app = express();
+
+app.use(history({
+    verbose: true
+}));
+app.use('/', express.static(path.join(__dirname, 'dist')));
+
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
