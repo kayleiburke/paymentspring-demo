e9c175125f8fa205020c604d50e869b2f99dd6cc
Kaylei Burke <kaylei.burke@gmail.com>
2021-05-31 20:28:42 -0500

    Pass in authorization token in all headers

--- src/store/helpers.js
+++ src/store/helpers.js
@@ -1,4 +1,4 @@
 export const getHeaders = function (state) {
-    return {'X-User-Token': state.auth.token, 'X-User-Email': state.auth.currentUser.email}
+    return {'X-User-Token': state.auth.token, 'X-User-Email': state.auth.currentUser.email, 'Authorization': 'Token token=' + state.auth.token }
 }
 
